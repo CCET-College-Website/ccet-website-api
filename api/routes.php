0ee71f7bd53581a6214dc54bd3b980270f9c7983
Sahil Garg <me.sahil.gg@gmail.com>
2025-08-28 16:57:15 +0530

    feat: bloat

---
+++ api/routes.php
@@ -0,0 +1,27 @@
+<?php
+include '../server.php';
+
+header("Content-Type: application/json");
+
+$method = $_SERVER['REQUEST_METHOD'];
+$input = json_decode(file_get_contents('php://input'), true);
+
+switch ($method) {
+    case 'GET':
+        break;
+
+    case 'POST':
+        break;
+
+    case 'PUT':
+        break;
+
+    case 'DELETE':
+        break;
+
+    default:
+        break;
+}
+
+$conn->close();
+?>
